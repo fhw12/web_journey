5f12bd4be7bd5583280c29f0b8a8d405adc1315a
fhw12 <nikita.pymitron@gmail.com>
2024-07-26 22:09:03 +0700

    new functions

--- js/08_functions.js
+++ js/08_functions.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// function declaration
 function sayHello(){
     console.log("Hello!");
 }
@@ -19,3 +20,46 @@ function sum(a = 0, b = 1){
 console.log(sum(7, 3));
 console.log(sum(4));
 console.log(sum(undefined, 3));
+
+function consoleLogArguments(){
+    console.log(arguments);
+}
+
+consoleLogArguments(5, 'hi', 1000n, null);
+
+// function expressions
+const hi = function(){
+    console.log('hi!');
+}
+
+hi();
+
+// arrow function
+const sayHi = () => {
+    //console.log(arguments); // arguments are unavailable in the arrow function
+    console.log('Hi');
+}
+
+const plus = (a, b) => a + b;
+
+sayHi();
+console.log(plus(5, 2));
+
+// callback
+const showConfirm = (afterActions = () => {}) => {
+    const result = confirm();
+    afterActions(result)
+}
+
+const showResult = (result) => { console.log(result); }
+
+showConfirm(showResult);
+showConfirm();
+
+// return function
+const getFunctionHello = () => {
+    return () => { console.log("Hello"); }
+}
+
+const helloFunction = getFunctionHello();
+helloFunction();
