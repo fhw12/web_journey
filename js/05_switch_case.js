d5df0c2a89983810347a9b6746b5d8dadc429885
fhw12 <nikita.pymitron@gmail.com>
2024-07-26 19:55:31 +0700

    switch case

---
+++ js/05_switch_case.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const number = 2;
+
+switch (number) {
+    case 1:
+        console.log("One");
+        break;
+    case 2:
+        console.log("Two");
+        break;
+    case 3:
+        console.log("Three");
+        break;
+    default:
+        console.log("I don't know what is it");
+}
