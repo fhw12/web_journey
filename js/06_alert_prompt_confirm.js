51d1ffbc5739c7d63182b5e014b9cb1819ff87f2
fhw12 <nikita.pymitron@gmail.com>
2024-07-26 20:07:27 +0700

    alert, prompt, confirm

---
+++ js/06_alert_prompt_confirm.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// alert
+alert("Hello, World!");
+
+// prompt
+const username = prompt("Your username");
+console.log(username);
+
+// confirm
+const isReady = confirm("Are you ready to improve your English and programming skills?")
+if(isReady){
+    console.log("Cool, let's get started!");
+} else {
+    console.log("What are you waiting for?");
+}
